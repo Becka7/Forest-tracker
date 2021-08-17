0ffa32d57a40f3ee6485bda5f63d341d1d12c721
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-08-17 14:10:34 +0300

    tests for the classes

--- src/main/java/EndageredAnimals.java
+++ src/main/java/EndageredAnimals.java
@@ -3,7 +3,15 @@ public class EndageredAnimals implements DatabaseManagement{
     private int id;
     private String name;
     private String age;
-    private String health;
+
+    private String healthy;
+    private String ill;
+    private String okay;
+
+    private String newborn;
+    private String young;
+    private String adult;
+
 
 
     public EndageredAnimals(int id,String name,String health,String age){
