bb643ab77a320fdd668f3fd6c2e335afb956e243
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-08-20 10:34:45 +0300

    Added pom.xml

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,5 +12,34 @@ class AnimalTest {
 
     @AfterEach
     void tearDown() {
+    }
+
+
+    @Test
+    public void NewHeroObjectedIsCreatedSuccessfully() {
+        Animal animal = new Animal("Thor", "young", "okay");
+        assertEquals(true, true);
+    }
+
+    public Animal setUpInstanceOf_NewAnimal() {
+        Animal animal= new Animal("KInkachu", "old", "ill");
+        return animal;
+    }
+
+    public Animal setUpAnotherAnimal() {
+        Animal animal = new Animal("monkey", "Newborn", "healthy");
+        return animal;
+    }
+
+    @Test
+    void AnimalName_true() {
+        Animal animal = setUpInstanceOf_NewAnimal();
+        assertEquals("KInkachu",animal.getName());
+    }
+
+    @Test
+    void AnimalHealthAsserts_true() {
+        Animal animal = setUpAnotherAnimal();
+        assertEquals("healthy",animal.getHealth());
     }
 }
